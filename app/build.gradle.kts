42bca7782356b1e1c12a9239da9e5bcc905108e1
Martin Lundberg <martin_o_lundberg@hotmail.com>
2024-06-14 15:46:38 -0500

    build: remove serialization plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.serialization)
     alias(libs.plugins.parcelize)
 }
 
