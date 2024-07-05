e60c3f4fb07de342e7405dc95ad4c76acf2994e8
Martin Lundberg <martin_o_lundberg@hotmail.com>
2024-07-04 21:27:44 -0500

    refactor: rephrase comment

--- app/src/main/java/net/martinlundberg/a1repmaxtracker/data/model/Movement.kt
+++ app/src/main/java/net/martinlundberg/a1repmaxtracker/data/model/Movement.kt
@@ -6,7 +6,7 @@ import net.martinlundberg.a1repmaxtracker.data.database.model.MovementEntity
 
 @Parcelize
 data class Movement(
-    val id: Long = 0, // Needs to be 0, will be overwritten by Room https://stackoverflow.com/questions/44109700/how-to-make-primary-key-as-autoincrement-for-room-persistence-lib#comment107992156_44109700
+    val id: Long = 0, // Needs to be 0 to be overwritten by Room https://stackoverflow.com/questions/44109700/how-to-make-primary-key-as-autoincrement-for-room-persistence-lib#comment107992156_44109700
     val name: String = "",
     val weight: Int? = null,
 ) : Parcelable
--- app/src/main/java/net/martinlundberg/a1repmaxtracker/data/model/OneRMInfo.kt
+++ app/src/main/java/net/martinlundberg/a1repmaxtracker/data/model/OneRMInfo.kt
@@ -4,7 +4,7 @@ import net.martinlundberg.a1repmaxtracker.data.database.model.OneRMEntity
 import java.time.OffsetDateTime
 
 data class OneRMInfo(
-    val id: Long = 0, // Needs to be 0, will be overwritten by Room https://stackoverflow.com/questions/44109700/how-to-make-primary-key-as-autoincrement-for-room-persistence-lib#comment107992156_44109700
+    val id: Long = 0, // Needs to be 0 to be overwritten by Room https://stackoverflow.com/questions/44109700/how-to-make-primary-key-as-autoincrement-for-room-persistence-lib#comment107992156_44109700
     val movementId: Long,
     val weight: Int,
     val offsetDateTime: OffsetDateTime,
