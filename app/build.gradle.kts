15910902df4609b61712ab4876de284d0ba2a306
Martin Lundberg <martin_o_lundberg@hotmail.com>
2024-07-14 12:47:25 -0500

    build: attempt generating native debug symbols

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,24 +12,34 @@ plugins {
 android {
     namespace = "net.martinlundberg.onerepmaxtracker"
     compileSdk = 34
+    //ndkVersion = "26.1.10909125" // specify ndkVersion to make AS generate native-debug-symbol.zip https://stackoverflow.com/a/76582634/2225594
 
     defaultConfig {
         applicationId = "net.martinlundberg.onerepmaxtracker"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 13
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
+
+        ndk {
+            debugSymbolLevel = "FULL"
+        }
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isShrinkResources = true
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+//            firebaseCrashlytics {
+//                nativeSymbolUploadEnabled = true
+//                unstrippedNativeLibsDir = file("build/app/intermediates/merged_native_libs/release/out/lib")
+//            }
         }
     }
     compileOptions {
@@ -49,7 +59,20 @@ android {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
+//        jniLibs.keepDebugSymbols += "**.so"
     }
+
+//    externalNativeBuild {
+//        cmake {
+//            cppFlags = "-std=c++17"
+//        }
+//    }
+
+//    externalNativeBuild {
+//        cmake {
+//            version = "3.10.2.4988404"
+//        }
+//    }
 }
 
 dependencies {
