0e67726a87c82d87a5cee78b266dcfa7c0eff700
Martin Lundberg <martin_o_lundberg@hotmail.com>
2024-07-25 13:25:01 -0500

    test: fix tests for ResultDetailScreen

--- app/src/androidTest/java/net/martinlundberg/onerepmaxtracker/feature/onerepmaxdetail/ResultDetailScreenTest.kt
+++ app/src/androidTest/java/net/martinlundberg/onerepmaxtracker/feature/onerepmaxdetail/ResultDetailScreenTest.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
+import net.martinlundberg.onerepmaxtracker.data.model.Percentage
 import net.martinlundberg.onerepmaxtracker.data.model.Result
 import net.martinlundberg.onerepmaxtracker.feature.onerepmaxdetail.ResultDetailUiState.Loading
 import net.martinlundberg.onerepmaxtracker.feature.onerepmaxdetail.ResultDetailUiState.Success
@@ -22,13 +23,12 @@ import java.time.OffsetDateTime
 import java.time.ZoneOffset
 
 @RunWith(AndroidJUnit4::class)
-@Ignore
-class OneRepMaxDetailScreenTest {
+class ResultDetailScreenTest {
 
     @get:Rule val composeTestRule = createComposeRule()
 
     @Test
-    fun givenMovementName_thenNameOfMovementIsDisplayed() {
+    fun givenLoading_thenLoadingIndicatorAndMovementNameAreDisplayed() {
         composeTestRule.setContent {
             ResultDetailScreen(
                 innerPadding = PaddingValues(),
@@ -38,24 +38,74 @@ class OneRepMaxDetailScreenTest {
         }
 
         composeTestRule.onNodeWithText("Name of movement").assertIsDisplayed()
-    }
-
-    @Test
-    fun givenStateIsLoading_thenLoadingIndicatorAndTextAreDisplayed() {
-        composeTestRule.setContent {
-            ResultDetailScreen(
-                innerPadding = PaddingValues(),
-                movementName = "Name of movement",
-                resultDetailUiState = Loading,
-            )
-        }
-
-        composeTestRule.onNodeWithText("Loading...").assertIsDisplayed()
         composeTestRule.onNodeWithContentDescription("Circular progress indicator").assertIsDisplayed()
     }
 
     @Test
-    fun givenResult_thenInfoIsDisplayed() {
+    fun givenResult_thenResultInfoIsDisplayed() {
+        composeTestRule.setContent {
+            ResultDetailScreen(
+                innerPadding = PaddingValues(),
+                movementName = "Name of movement",
+                resultDetailUiState = Success(
+                    result = Result(
+                        movementId = 55,
+                        weight = 100f,
+                        offsetDateTime = OffsetDateTime.of(2024, 7, 17, 0, 0, 0, 0, ZoneOffset.UTC),
+                        dateTimeFormatted = "Jul 17, 2024",
+                        comment = "",
+                    ),
+                    percentagesOf1RM = listOf(
+                        Percentage(90, 90),
+                        Percentage(85, 85),
+                    ),
+                    weightUnit = WeightUnit.KILOGRAMS,
+                )
+            )
+        }
+
+        composeTestRule.onNodeWithText("100 kg").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Jul 17, 2024").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Percentages of 1RM").assertIsDisplayed()
+        composeTestRule.onNodeWithText("90 %").assertIsDisplayed()
+        composeTestRule.onNodeWithText("90 kg").assertIsDisplayed()
+        composeTestRule.onNodeWithText("85 %").assertIsDisplayed()
+        composeTestRule.onNodeWithText("85 kg").assertIsDisplayed()
+    }
+
+    @Test
+    fun whenNavBackButtonIsClicked_thenNavigateBack() {
+        var navigateBackCalled = false
+        composeTestRule.setContent {
+            ResultDetailScreen(
+                innerPadding = PaddingValues(),
+                movementName = "Name of movement",
+                resultDetailUiState = Success(
+                    result = Result(
+                        movementId = 55,
+                        weight = 100f,
+                        offsetDateTime = OffsetDateTime.of(2024, 7, 17, 0, 0, 0, 0, ZoneOffset.UTC),
+                        dateTimeFormatted = "Jul 17, 2024",
+                        comment = "",
+                    ),
+                    percentagesOf1RM = listOf(
+                        Percentage(90, 90),
+                        Percentage(85, 85),
+                    ),
+                    weightUnit = WeightUnit.KILOGRAMS,
+                ),
+                navigateBack = {
+                    navigateBackCalled = true
+                }
+            )
+        }
+
+        composeTestRule.onNodeWithContentDescription("Back button").performClick()
+        assertTrue(navigateBackCalled)
+    }
+
+    @Test
+    fun whenEditIsClicked_thenEditResultDialogIsDisplayed() {
         composeTestRule.setContent {
             ResultDetailScreen(
                 innerPadding = PaddingValues(),
@@ -73,15 +123,13 @@ class OneRepMaxDetailScreenTest {
             )
         }
 
-        composeTestRule.onNodeWithText("Weight").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Date").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Time").assertIsDisplayed()
-        composeTestRule.onNodeWithContentDescription("Outlined Text Field Date Picker").assertIsDisplayed()
-        composeTestRule.onNodeWithContentDescription("Outlined Text Field Time Picker").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Edit").performClick()
+
+        composeTestRule.onNodeWithContentDescription("Edit result dialog").isDisplayed()
     }
 
     @Test
-    fun whenDateIsClicked_thenCalendarDialogIsDisplayed() {
+    fun whenDeleteIsClicked_thenDeleteResultConfirmDialogIsDisplayed() {
         composeTestRule.setContent {
             ResultDetailScreen(
                 innerPadding = PaddingValues(),
@@ -99,12 +147,14 @@ class OneRepMaxDetailScreenTest {
             )
         }
 
-        composeTestRule.onNodeWithContentDescription("Outlined Text Field Date Picker").performClick()
+        composeTestRule.onNodeWithText("Delete").performClick()
 
-        composeTestRule.onNodeWithContentDescription("Date Picker Dialog").isDisplayed()
+        composeTestRule.onNodeWithContentDescription("Delete result confirmation dialog").isDisplayed()
     }
 
+
     @Test
+    @Ignore
     fun givenCalendarDialog_whenCancelButtonIsClicked_thenDialogIsClosedAndUpdateIsNotCalled() {
         var updateOneRepMaxDetailCalled = false
         composeTestRule.setContent {
@@ -137,6 +187,7 @@ class OneRepMaxDetailScreenTest {
     }
 
     @Test
+    @Ignore
     fun givenCalendarDialog_whenAcceptButtonIsClicked_thenDialogIsClosedAndUpdateIsCalled() {
         var updateOneRepMaxDetailCalled = false
         composeTestRule.setContent {
@@ -166,97 +217,6 @@ class OneRepMaxDetailScreenTest {
         composeTestRule.onNodeWithText("Accept").performClick()
 
         composeTestRule.onNodeWithContentDescription("Date Picker Dialog").assertDoesNotExist()
-        assertTrue(updateOneRepMaxDetailCalled)
-    }
-
-    @Test
-    fun whenTimeIsClicked_thenTimePickerDialogIsDisplayed() {
-        composeTestRule.setContent {
-            ResultDetailScreen(
-                innerPadding = PaddingValues(),
-                movementName = "Name of movement",
-                resultDetailUiState = Success(
-                    result = Result(
-                        movementId = 55,
-                        weight = 100f,
-                        offsetDateTime = OffsetDateTime.of(2024, 7, 17, 0, 0, 0, 0, ZoneOffset.UTC),
-                        comment = "",
-                    ),
-                    percentagesOf1RM = emptyList(),
-                    weightUnit = WeightUnit.KILOGRAMS,
-                )
-            )
-        }
-
-        composeTestRule.onNodeWithContentDescription("Outlined Text Field Time Picker").performClick()
-
-        composeTestRule.onNodeWithContentDescription("Time Picker Dialog").isDisplayed()
-    }
-
-    @Test
-    fun givenTimePickerDialog_whenCancelButtonIsClicked_thenDialogIsClosedAndUpdateIsNotCalled() {
-        var updateOneRepMaxDetailCalled = false
-        composeTestRule.setContent {
-            ResultDetailScreen(
-                innerPadding = PaddingValues(),
-                movementName = "Name of movement",
-                resultDetailUiState = Success(
-                    result = Result(
-                        movementId = 55,
-                        weight = 100f,
-                        offsetDateTime = OffsetDateTime.of(2024, 7, 17, 0, 0, 0, 0, ZoneOffset.UTC),
-                        comment = "",
-                    ),
-                    percentagesOf1RM = emptyList(),
-                    weightUnit = WeightUnit.KILOGRAMS,
-                ),
-                updateResultDetail = { _, _ ->
-                    updateOneRepMaxDetailCalled = true
-                }
-            )
-        }
-
-        composeTestRule.onNodeWithContentDescription("Outlined Text Field Time Picker").performClick()
-        composeTestRule.onNodeWithContentDescription("Time Picker Dialog").isDisplayed()
-
-        composeTestRule.onNodeWithText("Cancel").performClick()
-
-        composeTestRule.onNodeWithContentDescription("Time Picker Dialog").assertDoesNotExist()
-        assertFalse(updateOneRepMaxDetailCalled)
-    }
-
-    @Test
-    fun givenTimePickerDialog_whenAcceptButtonIsClicked_thenDialogIsClosedAndUpdateIsCalled() {
-        var updateOneRepMaxDetailCalled = false
-        composeTestRule.setContent {
-            ResultDetailScreen(
-                innerPadding = PaddingValues(),
-                movementName = "Name of movement",
-                resultDetailUiState = Success(
-                    result = Result(
-                        movementId = 55,
-                        weight = 100f,
-                        offsetDateTime = OffsetDateTime.of(2024, 7, 17, 0, 0, 0, 0, ZoneOffset.UTC),
-                        comment = "",
-                    ),
-                    percentagesOf1RM = emptyList(),
-                    weightUnit = WeightUnit.KILOGRAMS,
-                ),
-                updateResultDetail = { _, _ ->
-                    updateOneRepMaxDetailCalled = true
-                }
-            )
-        }
-
-        composeTestRule.onNodeWithContentDescription("Outlined Text Field Time Picker").performClick()
-        composeTestRule.onNodeWithContentDescription("Time Picker Dialog").isDisplayed()
-
-        composeTestRule.onNodeWithContentDescription("5 o'clock").performClick()
-        composeTestRule.onNodeWithContentDescription("45 minutes").performClick()
-
-        composeTestRule.onNodeWithText("Accept").performClick()
-
-        composeTestRule.onNodeWithContentDescription("Time Picker Dialog").assertDoesNotExist()
         assertTrue(updateOneRepMaxDetailCalled)
     }
 }
