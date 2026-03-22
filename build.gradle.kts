fc923c9e34b879d4eaa3ef7bee65a619f0da5ffe
Martin Lundberg <martin_o_lundberg@hotmail.com>
2026-03-22 22:17:20 +0100

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.parcelize) apply false
-    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.hilt) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
 }
