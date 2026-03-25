afab18b960e3c0d73b32f4e51afc4c156e3dfae0
Martin Lundberg <martin_o_lundberg@hotmail.com>
2026-03-25 21:22:35 +0100

    bump versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "net.martinlundberg.onerepmaxtracker"
         minSdk = 26
         targetSdk = 34
-        versionCode = 14
+        versionCode = 15
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
